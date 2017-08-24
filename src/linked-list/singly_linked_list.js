5ce199f6a335f76488092c0f3162329c1941ae96
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-08-24 14:57:36 -0700

    updated set

--- src/linked-list/singly_linked_list.js
+++ src/linked-list/singly_linked_list.js
@@ -1,15 +1,15 @@
-function Node(val, next=null){
+function Node(val, next = null) {
   this.val = val;
   this.next = next;
 }
 
-function SinglyLinkedList(){
+function SinglyLinkedList() {
   this.head = null;
   this.tail = null;
   this.length = 0;
 }
 
-SinglyLinkedList.prototype.push = function(val){
+SinglyLinkedList.prototype.push = function(val) {
   let newNode = new Node(val);
   if (!this.head) {
     this.head = newNode;
@@ -22,10 +22,10 @@ SinglyLinkedList.prototype.push = function(val){
   return this
 }
 
-SinglyLinkedList.prototype._getNodeAt = function(index){
+SinglyLinkedList.prototype._getNodeAt = function(index) {
   let currentNode = this.head;
-  let count = 0 ;
-  while( count < index){
+  let count = 0;
+  while (count < index) {
     currentNode = currentNode.next;
     count += 1;
   }
@@ -38,8 +38,8 @@ SinglyLinkedList.prototype.pop = function() {
 
   let returnValue = this.tail.val;
 
-  if(this.length === 1) {
-    this.length = 0 ;
+  if (this.length === 1) {
+    this.length = 0;
     this.tail = null;
     this.head = null;
     return returnValue;
@@ -54,67 +54,87 @@ SinglyLinkedList.prototype.pop = function() {
 
 }
 
-SinglyLinkedList.prototype.clear = function (){
+SinglyLinkedList.prototype.clear = function() {
   this.head = null;
   this.tail = null;
   this.length = 0;
 }
 
-SinglyLinkedList.prototype.shift = function (){
+SinglyLinkedList.prototype.shift = function() {
   if (this.length === 0) {
     return 0;
   }
 
-  if(this.length >= 1) {
+  if (this.length >= 1) {
     let temp = this.head;
     this.head = this.head.next;
-    temp.next =  null;
-    this.length-- ;
+    temp.next = null;
+    this.length--;
     return temp.val;
   }
 }
 
-SinglyLinkedList.prototype.get = function(index){
-  if(index >= this.length){ return undefined; }
+SinglyLinkedList.prototype.get = function(index) {
+  if (index >= this.length) {
+    return undefined;
+  }
   let getIndex = this._getNodeAt(index);
 
   return getIndex.val;
 }
 
-SinglyLinkedList.prototype.remove = function(index){
-  if(!this.head) {
+SinglyLinkedList.prototype.remove = function(index) {
+  if (!this.head) {
     return undefined;
   }
-  if(this.length === 1){
+  if (this.length === 1) {
     let value = this.head.val;
     this.head = null;
     this.tail = null;
-    this.length = 0 ;
+    this.length = 0;
     return value;
   }
   let removedValue = this._getNodeAt(index);
-  let previousValue = this._getNodeAt(index-1);
+  let previousValue = this._getNodeAt(index - 1);
   let value = removedValue.val;
   previousValue.next = removedValue.next;
-  removedValue.next= null;
+  removedValue.next = null;
   this.length--;
   return value;
 }
 
-SinglyLinkedList.prototype.reverse = function () {
-  let next;
+SinglyLinkedList.prototype.reverse = function() {
   let current = this.head;
   let previous = null;
+  this.tail = this.head;
 
-  while(current){
+  while (current) {
     next = current.next;
     current.next = previous;
     previous = current;
     current = next;
   }
   this.head = previous;
+  return;
 };
 
+SinglyLinkedList.prototype.set = function(index, val) {
+  let current = this._getNodeAt(index);
+  current.val = val;
+};
 
+SinglyLinkedList.prototype.unshift = function(val) {
+  let newNode = new Node(val, this.head);
+
+  if (this.length === 0) {
+    this.head = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this.length;
+  }
+
+  this.head = newNode;
+  this.length += 1;
+}
 
 module.exports = SinglyLinkedList;
