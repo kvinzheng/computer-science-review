d29afe66ee498796410fcfb34a4a1851124cffd7
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-08-31 14:52:25 -0700

    updated mergesort

--- src/sorting/sortingAlgorithmsPart2.js
+++ src/sorting/sortingAlgorithmsPart2.js
@@ -6,32 +6,15 @@ const partition = require('./sort-helpers').partition;
 
 function mergeSort(items) {
   // Terminal case: 0 or 1 item arrays don't need sorting
-  if (items.length < 2) {
+  if(items.length < 2){
     return items;
   }
-
-  let middle = Math.floor(items.length / 2);
-  let left = items.slice(0, middle);
-  let right = items.slice(middle);
+  let midpoint = Math.floor(items.length/2);
+  let left = items.slice(0, midpoint);
+  let right = items.slice(midpoint);
   return merge(mergeSort(left), mergeSort(right));
 }
 
-// function quickSort(arr, left, right) {
-// let index;
-// if (arr.length > 1) {
-//
-//   left = typeof left !== "number" ? 0 : left;
-//   right = typeof right !== "number" ? arr.length - 1: right;
-//
-//   index = partition(arr, left, right);
-//   if (left < index - 1) {
-//     quickSort(arr, left, index - 1);
-//   }
-//   if( index < right) {
-//     quickSort(arr, index, right);
-//   }
-// }
-// return arr;
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left < right) {
     let p = partition(arr, left, right);
