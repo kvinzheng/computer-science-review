b438b6cbe9ca1cd8395ac6092cd1999046cf5506
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-09-13 18:40:22 -0700

    added test array to tree node structure

---
+++ src/trees/common_tree_problems/test/array_to_tree.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const arrayToTree = require('../array_to_tree.js');
+
+describe("arrayToTree", function() {
+
+  it("empty array", function() {
+    var array = [];
+    var expected = undefined;
+    expect(arrayToTree(array)).to.equal(undefined);
+  });
+
+  it("array with multiple elements", function() {
+    var array = [17, 0, -4, 3, 15];
+    var expected = new TreeNode(17, new TreeNode(0, new TreeNode(3), new TreeNode(15)), new TreeNode(-4));
+    expect(arrayToTree(array)).to.equal(undefined);
+  });
+});
