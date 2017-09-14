fc9a3f1dae841974ca4f3ecf7262efd546cacde0
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-09-13 18:30:18 -0700

    finished sum the tree

--- src/trees/common_tree_problems/sum_the_tree.js
+++ src/trees/common_tree_problems/sum_the_tree.js
@@ -16,9 +16,24 @@
 //      2
 // => 3
 
-function sum_the_tree (node){
-  let current = this.node;
+function sum_the_tree(node) {
+  let current = node;
+  let sum = 0;
+  let queue = [];
+  queue.push(current);
 
+  while (queue.length > 0) {
+    let current = queue.shift();
+    sum += current.value;
+
+    if (current.left) {
+      queue.push(current.left);
+    }
+    if (current.right) {
+      queue.push(current.right);
+    }
+  }
+  return sum;
 }
 
 module.exports = sum_the_tree;
